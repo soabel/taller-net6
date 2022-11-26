bcdff4d97adae812532a75d5e40d2a01be4e9418
soabel <alfredo.benaute@gmail.com>
2022-11-25 19:19:44 -0500

    fix: add project

---
+++ modulos 1-2/todo-api-minimal/Model/TodoContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace todo_api_minimal.Model
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItem { get; set; }
+
+    }
+}
+
---
+++ modulos 1-2/todo-api-minimal/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using todo_api_minimal.Model;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.MapGet("/todo", async (TodoContext context) => await context.TodoItem.ToListAsync());
+
+app.MapGet("/todo/{id}", async (TodoContext context, int id) => await context.TodoItem.FirstOrDefaultAsync(x => x.Id == id));
+
+app.MapPost("/todo", async (TodoContext context, TodoItem item) => {
+    context.TodoItem.Add(item);
+    await context.SaveChangesAsync();
+});
+
+
+app.Run();
